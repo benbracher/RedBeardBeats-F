7430bc9effa56c3f5599041b57b47dc3f73837f8
Avery Vann <danny@dougvann.com>
2019-01-15 16:43:11 -0500

    added auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RegistrationComponent } from './components/register/register.component';
 import { SongComponent } from './components/song/song.component';
 import { PlaylistComponent } from './components/playlist/playlist.component';
 import { PlaylistCollectionComponent } from './components/playlist-collection/playlist-collection.component';
 import { AdminPortalComponent } from './components/admin-portal/admin-portal.component';
 import { PlayControlsComponent } from './components/play-controls/play-controls.component';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatTableModule
+}
+  from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -19,16 +30,25 @@ import { PlayControlsComponent } from './components/play-controls/play-controls.
     SidenavComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent,
     SongComponent,
     PlaylistComponent,
     PlaylistCollectionComponent,
     AdminPortalComponent,
-    PlayControlsComponent
+    PlayControlsComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
