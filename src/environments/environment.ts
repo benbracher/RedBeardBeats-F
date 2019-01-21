9be82848c64b016187a01b2fdfcc6b1e9243aecf
Avery Vann <danny@dougvann.com>
2019-01-21 16:40:46 -0500

    effin work b

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  serverUrl: 'https://rbbeatsapi.azurewebsites.net'  
+   serverUrl: 'https://rbbeatsapi.azurewebsites.net'  
 };
 
 /*
